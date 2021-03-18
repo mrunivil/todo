ad9adce7868a56935ff7e046b3c7f27b945157ce
mrunivil <mr.univil@googlemail.com>
2021-03-18 10:34:06 +0100

    refactoring

--- src/app/features/todos/save/save.todo.module.ts
+++ src/app/features/todos/save/save.todo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
-import { AbstractSaveToDoDataSource } from "./data/abstract.save.todo.data.source";
+import { AbstractSaveToDoDataSource } from "./data-source/abstract.save.todo.data.source";
 import { DefaultSaveToDoDataSource } from "./data/default.save.todo.data.source";
-import { AbstractSaveToDoRepository } from "./domain/abstract.save.todo.repository";
-import { AbstractSaveToDoUseCase } from "./domain/abstract.save.todo.use.case";
-import { DefaultSaveToDoRepository } from "./domain/default.save.todo.repository";
-import { DefaultSaveTodoUseCase } from "./domain/default.save.todo.use.case";
+import { AbstractSaveToDoRepository } from "./repository/abstract.save.todo.repository";
+import { AbstractSaveToDoUseCase } from "./use-case/abstract.save.todo.use.case";
+
+import { DefaultSaveToDoRepository } from "./repository/default.save.todo.repository";
+import { DefaultSaveTodoUseCase } from "./use-case/default.save.todo.use.case";
 
 
 @NgModule({
